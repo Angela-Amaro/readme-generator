e397462380a20ebc6aac86ccfc84d141f2384949
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-21 02:42:21 -0500

    added to starter code, added inquirer, created a gitignore, installed npm

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+{
+    type: 'input',
+    name: 'title',
+    message: 'what is the title of your repository?'
+},
+{
+    type: 'input',
+    name: 'author',
+    message: 'list your name here'
+},
+{
+    type: 'input',
+    name: 'sections',
+    message: 'What sections do you have?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'what is the description of your project?',
+},
+//table of contents 
+{
+    type: 'input',
+    name: 'installation',
+    message: 'what installation steps does the user need in order to run your project?',
+},
+{
+    type: 'input',
+    name: 'Usage',
+    message: 'what usage does your project propose?',
+},
+{
+    type: 'list',
+    name: 'License',
+    message: 'select your license',
+    choices: ['MIT License','GNU General Public License v3.0', 'The Unlicense', 'Mozilla Public License 2.0'],
+},
+{
+    type: 'input',
+    name: 'contributing',
+    meessage: 'list contributors to your project here'
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'put tests here'
+},
+{
+    type: 'input',
+    name: 'questions',
+    message: 'In case people have questions list your linkadin here'
+},];
+// // TODO: Create a function to write README file
+function writeToFile(genreadme,data) {   
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+fs.writeToFile(genreadme, data, (err) =>
+err ? console.log(err) : console.log('Your generated readme is complete')
+); 
+}
+function init() {
+inquirer
+    .prompt(questions)
+    .then(function(answers) {
+    const initmarkdowncontent = markdown.genmarkdown(answers)
+    const genReadMeFile = 'README.md'
+    writeToFile(initmarkdowncontent, genReadMeFile)
+})
+}
+init();
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
-init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+ return "![repository]('https://img.shields.io/github/license/<Github-Username>/<Repository>')" + license
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license){
+  return  "https://opensource.org/licenses/" + license
+  }
+  else {
+  return "";
+}
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  //fotmat readme here https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
   return `# ${data.title}
-
+  ${renderLicenseBadge}
+  ${renderLicenseLink}
+  ${renderLicenseSection}
 `;
 }
 
